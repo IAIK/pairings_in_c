95f8d5f500a64f4c438c670c6e20a52f6a5c5208
Thomas Unterluggauer <thomas.unterluggauer@iaik.tugraz.at>
2015-09-18 08:49:11 +0200

    removed build files and moved scripts

--- framework/generate_dump.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-mkdir build/dump
-arm-none-eabi-objdump -S $1 > build/dump/main_main.dump
-arm-none-eabi-objdump -x $1 > build/dump/header_main.dump
-arm-none-eabi-nm -nS $1 > build/dump/main.dump
-arm-none-eabi-objcopy -O ihex $1 build/dump/main.ihex
-arm-none-eabi-size $1 > build/dump/main.size
